0c4d213b21b5c803ea46d52e219bf7de39e6ecd9
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-11 13:14:46 -0400

    Refactored and added comments

--- hangman.rb
+++ hangman.rb
@@ -1,22 +1,22 @@
-require 'yaml'
+require 'yaml' #require the yaml libary to save and load games
 
-class Hangman
+class Hangman 
 	
 	def initialize
-		@game_over = false
-		@body_parts = ["Right arm", "Left arm", "Right leg", "Left leg", "Head", "Torso"]
+		@game_over = false #set the game_over variable to false
+		@body_parts = ["right arm", "left arm", "right leg", "left leg", "head", "torso"] #set body parts
+		@previous_guesses =[]
 		select_word
-		welcome
 	end
 
-	def play
+	def play #method to play the game
 		until @game_over == true
 			guess
 		end
-		end_game
+		puts "The word was #{@random_word}"
 	end
 
-	def select_word
+	def select_word #open dictionary file and store all words between 5 and 12 characters in the words array
 		words = []
 		lines = File.readlines "5desk.txt"
 		lines.each do |line|
@@ -26,65 +26,62 @@ class Hangman
 			end
 		end
 
-		@random_word = words.sample.downcase
-		@hidden_word = "-" * @random_word.length
+		@random_word = words.sample.downcase #use sample method to store a random object from the words array
+		@hidden_word = "-" * @random_word.length #display "-"" for every charachter of the random_word to display to user
 		
-	end
-	
-	def welcome
-		puts "Welcome to Hangman!"
 	end
 
 	def guess
 		puts @hidden_word
-		puts "You still have the following body parts: #{@body_parts}"
+		puts "Letters guessed: #{@previous_guesses}"
+		puts "You have the following body parts: #{@body_parts}"
 		puts "Guess a letter! or type 'save' to save the game."
-		letter_guess = gets.chomp.downcase
+		letter_guess = gets.chomp.downcase #get guess input from the user
 
-		if letter_guess == 'save'
+		if letter_guess == 'save' 
 			save
-			exit(0)
-		elsif !letter_guess.match(/[[:alpha:]]/)
+			exit(0) #exit out of loop
+		elsif !letter_guess.match(/[[:alpha:]]/) #if input is not a letter
 			puts "That is not a letter. Try again!"
 			guess
-		elsif letter_guess.length != 1
+		elsif letter_guess.length != 1 #if the input is not exactly 1 character
 			puts "You must guess one letter. Try again!"
 			guess
-		else
-			position = 0
-			safe = false
-			@random_word.split("").each do |x|
-				if letter_guess == x
-					@hidden_word[position] = letter_guess
-					safe = true
+		elsif @previous_guesses.include?(letter_guess)
+			puts "You already guessed that letter. Try again!"
+		#if input is valid for each random_word if the guessed letter is equal to the actual letter replace it in the hidden word
+		else 
+			position = 0 
+			lose_body_part = true 
+			@random_word.split("").each do |letter|
+				if letter_guess == letter
+					@hidden_word[position] = letter 
+					lose_body_part = false #do not execute lose_body_part
 				end
-				position = position + 1
+				position += 1
 			end
+			@previous_guesses.push(letter_guess)
 		end
 
-		if @random_word == @hidden_word
+		if @random_word == @hidden_word #end the game if the user has revealed the word
 			@game_over = true
 			puts "You won!"
 		end
 
-		if safe == false
-			body_part = @body_parts.sample
+		if lose_body_part == true #remove random body part
+			body_part = @body_parts.sample 
 			puts "That letter is not in the word. Sorry you lost your #{body_part}!"
-			@body_parts.delete(body_part)
+			@body_parts.delete(body_part) 
 		end
 
-		if @body_parts.empty?
+		if @body_parts.empty? #end the game if there are no more body parts
 			@game_over = true
 			puts "You lost!"
 		end
 	end
-	def end_game
-		puts "The word was #{@random_word}"
-		exit(0)
-	end
 end
 
-def save
+def save #save the game as a yaml file into the games directory
 	Dir.mkdir('games') unless Dir.exist? 'games'
 	name = 'games/saved.yaml'
 	File.open(name, 'w') do |file|
@@ -93,21 +90,21 @@ def save
 	puts "Game has been saved!"
 end
 
-def load
-	puts "Do you want to load an old game? (y/n)"
+def start_game 
+	puts "Welcome to Hangman! Do you want to load an old game? (y/n)"
 	old_game = gets.chomp
-	if old_game == "y"
+	if old_game == "y" #load old game
 		content = File.open('games/saved.yaml', 'r') {|file| file.read }
 		game = YAML.load(content) 
 		game.play
-	elsif old_game == "n"
+	elsif old_game == "n" #start new game
 		game = Hangman.new
 		game.play
 	else
 		puts "That's not an option. Try again!"
-		load
+		start_game
 	end
 end
 
-load
+start_game
 
